359f6049445e1710ba35b80cb80f17fa47726f1b
thanhvinh8192 <thanhvinh8192@gmail.com>
2021-01-17 23:42:33 +0900

    Update Activity 3.1

--- src/Activity_31/Application.java
+++ src/Activity_31/Application.java
@@ -5,6 +5,7 @@ import Activity_31.Service.LaptopService;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
@@ -26,8 +27,17 @@ public class Application {
             System.out.println("SQL Connection to database established!");
 
             LaptopService laptopService = new LaptopService(connection);
-            List<Laptop> laptops = laptopService.findLaptopByPrice(null, 3000000f);
-            System.out.println("test connection " + laptops.size());
+            /*List<Laptop> laptops = laptopService.findLaptopByPrice(null, 30000000f);
+            for (Laptop laptop :laptops) {
+                System.out.println(laptop.toString());
+            }*/
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Nhap ten hang san xuat: ");
+            String brandName = scanner.nextLine();
+            System.out.println();
+            System.out.print("Nhap loai o cung (TRUE = ssd, FALSE = hdd): ");
+            boolean type = scanner.nextBoolean();
+            List<Laptop> laptops = laptopService.findLaptopByMaker(brandName, type);
             for (Laptop laptop :laptops) {
                 System.out.println(laptop.toString());
             }
--- src/Activity_31/Service/LaptopService.java
+++ src/Activity_31/Service/LaptopService.java
@@ -9,6 +9,7 @@ import java.sql.Statement;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class LaptopService {
     Connection connection;
@@ -48,7 +49,47 @@ public class LaptopService {
 
                 Laptop laptop = new Laptop(name, url, maker, type, ram, cpu, ssd, hdd, price, card, screen_resolution, screen_size, sold, created_timestamp, last_updated_timestamp);
                 response.add(laptop);
-                System.out.println("da them" + response.size());
+            }
+        }
+        catch (Exception e){
+            System.out.println("ERROR: " +e);
+        }
+        return response;
+    }
+
+    public List<Laptop> findLaptopByMaker(String brandName, boolean typeHardDisk){
+        List<Laptop> response = new ArrayList<>();
+        try {
+            String query = "SELECT * FROM laptop WHERE TRUE";
+            if (brandName!= null){
+                query = query + " AND maker = '" + brandName + "'";
+            }
+            if (typeHardDisk){
+                query = query + " AND ssd IS NOT NULL";
+            }else {
+                query = query + " AND hdd IS NOT NULL";
+            }
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(query);
+            while (rs.next()) {
+                String name = rs.getString("name");
+                String url = rs.getString("url");
+                String maker = rs.getString("maker");
+                String type = rs.getString("type");
+                String ram = rs.getString("ram");
+                String cpu = rs.getString("cpu");
+                String ssd = rs.getString("ssd");
+                String hdd = rs.getString("hdd");
+                float price = rs.getFloat("price");
+                String card = rs.getString("card");
+                String screen_resolution = rs.getString("screen_resolution");
+                float screen_size = rs.getFloat("screen_size");
+                int sold = rs.getInt("sold");
+                Timestamp created_timestamp = rs.getTimestamp("created_timestamp");
+                Timestamp last_updated_timestamp = rs.getTimestamp("last_updated_timestamp");
+
+                Laptop laptop = new Laptop(name, url, maker, type, ram, cpu, ssd, hdd, price, card, screen_resolution, screen_size, sold, created_timestamp, last_updated_timestamp);
+                response.add(laptop);
             }
         }
         catch (Exception e){
